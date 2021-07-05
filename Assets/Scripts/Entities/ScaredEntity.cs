fb4c9b9b6d647a4d1e0ed0ce70d378eecfd578f4
CatSandwich <59626141+CatSandwich@users.noreply.github.com>
2021-07-05 03:16:08 -0400

    ScaredEntity now blushes via co-routine

--- Assets/Scripts/Entities/ScaredEntity.cs
+++ Assets/Scripts/Entities/ScaredEntity.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Entities
@@ -5,12 +6,40 @@ namespace Entities
     public class ScaredEntity : EntityBase, IPlayerReactive
     {
         public float ScaredSpeed;
+        public float ScaredTransitionTime;
+        public Color ScaredColor;
+        public SpriteRenderer SR;
+        
         private bool _isReacting;
+        
+        private float _reactionStartTime;
+        private Color _reactionStartColor;
+        
+        public void Update()
+        {
+            // Set the color
+            if (!_isReacting) return;
+            SR.color = Color.Lerp(
+                _reactionStartColor, 
+                ScaredColor, 
+                (Time.time - _reactionStartTime) / ScaredTransitionTime);
+        }
+        
         public void React(PlayerController player)
         {
             if (_isReacting) return;
             _isReacting = true;
-            RB.velocity += (Vector2)(transform.position - player.transform.position).normalized * ScaredSpeed;
+            _reactionStartTime = Time.time;
+            _reactionStartColor = SR.color;
+            StartCoroutine(_run(player));
+        }
+
+        private IEnumerator _run(PlayerController player)
+        {
+            yield return new WaitForSeconds(ScaredTransitionTime);
+            RB.velocity += (Vector2) (
+                    transform.position - 
+                    player.transform.position).normalized * ScaredSpeed;
         }
     }
 }
